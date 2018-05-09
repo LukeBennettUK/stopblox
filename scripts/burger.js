f20769c60981312c73cc3e624ff5ee695a47f61a
LukeBennettUK <luke5495@gmail.com>
2018-05-09 10:51:37 +0100

    File Upload
    File Upload

---
+++ scripts/burger.js
@@ -0,0 +1,10 @@
+function openSideMenu() {
+  document.getElementById('side-menu').style.width = '100%';
+  document.getElementById('side-menu').style.maxWidth = '100%';
+  document.getElementById('side-menu').style.marginRight = '0';
+}
+
+function closeSideMenu() {
+  document.getElementById('side-menu').style.width = '0';
+  document.getElementById('side-menu').style.marginLeft = '0';
+}
